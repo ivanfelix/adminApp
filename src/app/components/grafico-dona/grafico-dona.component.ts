fe21c332cf845cf2ae3620f5b161e53bdbd1071c
Herbax de Mexico <herbax@iMac-de-Herbax.local>
2018-09-18 10:12:32 -0500

    componentes padres e  hijos

--- src/app/components/grafico-dona/grafico-dona.component.ts
+++ src/app/components/grafico-dona/grafico-dona.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-grafico-dona',
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraficoDonaComponent implements OnInit {
 
-  public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData: number[] = [350, 450, 100];
-  public doughnutChartType: string = 'doughnut';
+  @Input('chartLabels') doughnutChartLabels: string[] = [];
+  @Input('chartData') doughnutChartData: number[] = [];
+  @Input('chartType') doughnutChartType: string = '';
 
   constructor() { }
 
